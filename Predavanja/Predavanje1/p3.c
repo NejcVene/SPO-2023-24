19b778f4357e1547d46f964b2b171798e22c7103
NejcVene <nejcvene.nv@gmail.com>
2023-10-04 15:50:49 +0200

    Made changes.

--- Predavanja/Predavanje1/p3.c
+++ Predavanja/Predavanje1/p3.c
@@ -7,7 +7,26 @@ v[i][j] = *(*(v + i) + j) je isto
 
 v[0][0] = *v = **v je isto
 */
-
+/*
+Alokacija:
+int **p = (int **) malloc(N * sizeof(int *))
+if (p == NULL) {
+    napaka
+}
+for (int i = 0; i<N; i++) {
+    p[i] = (int *) malloc(N * sizeof(int));
+    if (p[i] == NULL) {
+        napaka
+    }
+}
+dealokacija:
+for (int i = 0; i<N; i++) {
+    free(p[i]);
+}
+if (p) { pogleda, če obstaja (je različen od NULL)
+    free(p)
+} 
+*/
 int main(void) {
 
     int v[2][3] = {{10, 20, 30}, {50, 60, 70}};
