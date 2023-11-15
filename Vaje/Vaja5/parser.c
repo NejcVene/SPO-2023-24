e63b130da92f2bc7fe78d5fd4258b0fa09c4cb86
NejcVene <nejcvene.nv@gmail.com>
2023-11-15 15:10:52 +0100

    Parser can now parse multiple tabs & spaces

--- Vaje/Vaja5/parser.c
+++ Vaje/Vaja5/parser.c
@@ -3,18 +3,39 @@
 #include <string.h>
 
 int parse(char *, char *[]);
+int parse2(char *, char *[]);
 
 int main(void) {
 
     char *args[64];
-    char str[] = " gcc  -o   main main.c -Wall -lm -lpthead -fopenmp";
-    int el = parse(str, args);
+    char str[] = "          gcc  -o  main main.c -Wall -lm -lpthead -fopenmp";
+    int el = parse2(str, args);
 
     for (int i = 0; i<el; i++) {
         printf("%s (%p)\n", args[i], &args[i]);
     }
 
     return 0;
+
+}
+
+int parse2(char command[], char *args[]) {
+
+    int commandIndex = 0, argsIndex = 0, isEmpty = 0;
+    while (command[commandIndex] != '\0') {
+        if (command[commandIndex] == ' ' || command[commandIndex] == '\t') {
+            command[commandIndex] = '\0';
+            isEmpty = 0;
+        } else {
+            if (isEmpty == 0) {
+                args[argsIndex++] = &command[commandIndex];
+                isEmpty = 1;
+            }
+        }
+        commandIndex++;
+    }
+
+    return argsIndex;
 
 }
 
